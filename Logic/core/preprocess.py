2a680f411a5fb98c8fe343208542f1321b13a097
amir-haji <haji80as@gmaiil.com>
2024-03-23 13:57:05 +0330

    complete preprocess

--- Logic/core/preprocess.py
+++ Logic/core/preprocess.py
@@ -1,3 +1,9 @@
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+import contractions
+import unidecode
+import string
+import re
 
 
 class Preprocessor:
@@ -12,8 +18,14 @@ class Preprocessor:
             The list of documents to be preprocessed, path to stop words, or other parameters.
         """
         # TODO
+        with open('stopwords.txt', 'r') as f:
+            txt = f.read()
+            f.close()
+            
+        self.stemmer = PorterStemmer()
+        self.lemmatizer = WordNetLemmatizer()
         self.documents = documents
-        self.stopwords = []
+        self.stopwords = txt.split('\n')
 
     def preprocess(self):
         """
@@ -25,6 +37,21 @@ class Preprocessor:
             The preprocessed documents.
         """
          # TODO
+        for doc in self.documents:
+            for k in doc:
+                if isinstance(doc[k], list):
+                    new_item = []
+                    if len(doc[k]) != 0:
+                        if isinstance(doc[k][0], list):
+                            # reviews
+                            for review, score in doc[k]:
+                                new_item.append([self.normalize(review), score])
+                        else:
+                            new_item = [self.normalize(text) for text in doc[k]]
+                    doc[k] = new_item
+                else:
+                    doc[k] = self.normalize(doc[k])
+                    
         return
 
     def normalize(self, text: str):
@@ -42,7 +69,32 @@ class Preprocessor:
             The normalized text.
         """
         # TODO
-        return
+
+        text = unidecode.unidecode(text)
+        text = text.lower()
+        """
+        contractions = {r'we[\’\']re': 'we are',
+                        r'do[\’\']nt': 'do not',
+                        r'ca[\’\']nt': 'cannot',
+                        r'is[\’\']nt': 'is not',
+                       r'does[\’\']nt': 'does not'}
+        
+        for k, v in contractions.items():
+            text = re.sub(k, v, text)
+        """
+        text = re.sub(r'\n', ' ', text)
+        text = re.sub(r'\s+', ' ', text)
+        text.strip()
+
+        sentences = sent_tokenize(text)
+        normalized_text = ''
+        for sentence in sentences:
+            sent = self.remove_links(sentence)
+            sent = self.remove_punctuations(sent)
+            words = self.remove_stopwords(sent)
+            normalized_text = normalized_text + ' ' + ' '.join(words)
+        
+        return normalized_text
 
     def remove_links(self, text: str):
         """
@@ -60,7 +112,10 @@ class Preprocessor:
         """
         patterns = [r'\S*http\S*', r'\S*www\S*', r'\S+\.ir\S*', r'\S+\.com\S*', r'\S+\.org\S*', r'\S*@\S*']
         # TODO
-        return
+
+        for pattern in patterns:
+            text = re.sub(pattern, '', text)
+        return text
 
     def remove_punctuations(self, text: str):
         """
@@ -77,7 +132,9 @@ class Preprocessor:
             The text with punctuations removed.
         """
         # TODO
-        return
+        punctuation_table = str.maketrans(string.punctuation, ' '*len(string.punctuation))
+        text = text.translate(punctuation_table)
+        return text
 
     def tokenize(self, text: str):
         """
@@ -94,7 +151,7 @@ class Preprocessor:
             The list of words.
         """
         # TODO
-        return
+        return word_tokenize(text)
 
     def remove_stopwords(self, text: str):
         """
@@ -111,5 +168,10 @@ class Preprocessor:
             The list of words with stopwords removed.
         """
         # TODO
-        return
+        words = self.tokenize(text)
+        final_words = []
+        for w in words:
+            if w not in self.stopwords:
+                final_words.append(contractions.fix(self.lemmatizer.lemmatize(w)))
+        return final_words
 
