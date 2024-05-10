f468df549a2e60ee50ca5a75a66fbf53ae7ffe41
amir-haji <haji80as@gmaiil.com>
2024-05-10 22:24:08 +0330

    complete Link Analyzer part

--- Logic/core/link_analysis/analyzer.py
+++ Logic/core/link_analysis/analyzer.py
@@ -1,6 +1,9 @@
-from .graph import LinkGraph
-from ..indexer.indexes_enum import Indexes
-from ..indexer.index_reader import Index_reader
+from graph import LinkGraph
+import sys
+sys.path.append('/Users/hajmohammadrezaee/Desktop/MIR-Project/Logic/core')
+from indexer.indexes_enum import Indexes
+from indexer.index_reader import Index_reader
+import json
 
 class LinkAnalyzer:
     def __init__(self, root_set):
@@ -30,8 +33,14 @@ class LinkAnalyzer:
         This function has no parameters. You can use self to get or change attributes
         """
         for movie in self.root_set:
-            #TODO
-            pass
+            id, title, stars = list(movie.values())
+            self.graph.add_node(title)
+            self.hubs.append(title)
+            for star in stars:
+                self.graph.add_node(star)
+                self.authorities.append(star)
+                self.graph.add_edge(title, star)
+            
 
     def expand_graph(self, corpus):
         """
@@ -50,8 +59,27 @@ class LinkAnalyzer:
         and refer to the nodes in the root set to the graph and to the list of hubs and authorities.
         """
         for movie in corpus:
-            #TODO
-            pass
+            id, title, stars = list(movie.values())
+            
+            if title in self.hubs:
+                continue
+
+            add_movie = False
+            for star in stars:
+                if star in self.authorities:
+                    add_movie = True
+                    break
+                    
+            if add_movie:
+                self.graph.add_node(title)
+                self.hubs.append(title)
+                for star in stars:
+                    if star not in self.authorities:
+                        self.graph.add_node(star)
+                        self.authorities.append(star)
+                    self.graph.add_edge(title, star)
+                    
+            
 
     def hits(self, num_iteration=5, max_result=10):
         """
@@ -71,17 +99,57 @@ class LinkAnalyzer:
         list
             List of names of 10 movies with the most scores obtained by Hits algorithm in descending order
         """
-        a_s = []
-        h_s = []
+        a_s = dict(zip(self.authorities, [1] * len(self.authorities)))
+        h_s = dict(zip(self.hubs, [1] * len(self.hubs)))
 
-        #TODO
-
+        
+        for _ in range(num_iteration):
+            for star in self.authorities:
+                movies = self.graph.get_predecessors(star)
+                for movie in movies:
+                    h_s[movie] += a_s[star]
+                    
+            for movie in self.hubs:
+                stars = self.graph.get_successors(movie)
+                for star in stars:
+                    a_s[star] += h_s[movie]
+                    
+            sum_h = sum(list(h_s.values()))
+            sum_a = sum(list(a_s.values()))
+            
+            a_s.update((x, y/sum_a) for x, y in a_s.items())
+            h_s.update((x, y/sum_h) for x, y in h_s.items())
+            
+        a_s = sorted(a_s.items(), key = lambda x: (x[1], x[0]), reverse = True)[:max_result]
+        h_s = sorted(h_s.items(), key = lambda x: (x[1], x[0]), reverse = True)[:max_result]
+        
+        a_s = [x[0] for x in a_s]
+        h_s = [x[0] for x in h_s]
+        
         return a_s, h_s
+
+
 
 if __name__ == "__main__":
     # You can use this section to run and test the results of your link analyzer
-    corpus = []    # TODO: it shoud be your crawled data
-    root_set = []   # TODO: it shoud be a subset of your corpus
+    with open('../IMDB_crawled.json', 'r') as f:
+        data = json.loads(f.read())
+        f.close() 
+
+    root_set = []
+    for movie in data:
+        if 'Batman' in movie['title']:
+            root_set.append(movie)  
+    
+    root_set = [{'id': movie['id'],\
+                  'title': movie['title'],\
+                      'stars': [] if movie['stars'] is None else movie['stars']}\
+                          for movie in root_set]
+    
+    corpus = [{'id': movie['id'],\
+                'title': movie['title'],\
+                      'stars': [] if movie['stars'] is None else movie['stars']}\
+                          for movie in data]
 
     analyzer = LinkAnalyzer(root_set=root_set)
     analyzer.expand_graph(corpus=corpus)
--- Logic/core/link_analysis/graph.py
+++ Logic/core/link_analysis/graph.py
@@ -1,4 +1,4 @@
-import networkx as nx
+# import networkx as nx
 
 class LinkGraph:
     """
@@ -6,22 +6,25 @@ class LinkGraph:
     You are free to modify this class according to your needs.
     You can add or remove methods from it.
     """
+    
     def __init__(self):
-        #TODO
-        pass
+        self.node_ids = []
+        self.successors = {}
+        self.predecessors = {}
+        
+    def add_edge(self, u: str, v: str):
+        if u in self.node_ids and v in self.node_ids:
+            self.successors[u].append(v)
+            self.predecessors[v].append(u)
 
-    def add_edge(self, u_of_edge, v_of_edge):
-        #TODO
-        pass
+    def add_node(self, node: str):
+        if not(node in self.node_ids):
+            self.node_ids.append(node)
+            self.successors[node] = []
+            self.predecessors[node] = []
 
-    def add_node(self, node_to_add):
-        #TODO
-        pass
+    def get_successors(self, node: str):
+        return self.successors[node]
 
-    def get_successors(self, node):
-        #TODO
-        pass
-
-    def get_predecessors(self, node):
-        #TODO
-        pass
+    def get_predecessors(self, node: str):
+        return self.predecessors[node]
